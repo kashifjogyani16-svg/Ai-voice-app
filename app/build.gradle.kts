fa52910dc7ec5386d2271fbfa7ef6ca5167301a3
kashifjogyani16-svg <kashifjogyani16@gmail.com>
2026-09-24 01:57:15 +0500

    Fix Gradle plugin resolution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,5 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
