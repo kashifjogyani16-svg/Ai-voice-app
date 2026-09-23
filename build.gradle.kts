71463644f900b883662f9f69a014751f9e12eeb6
kashifjogyani16-svg <kashifjogyani16@gmail.com>
2026-09-24 02:02:11 +0500

    Switch root build.gradle.kts to plugins{} DSL to match settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,7 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
-        classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.0.21")
-    }
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
 
 tasks.register("clean", Delete::class) {
